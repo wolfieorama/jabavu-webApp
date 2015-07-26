b500753bbe6bb48cf6e14640890196a3bf0035fe
wolfieorama <wachirawamunyi@gmail.com>
2015-07-26 22:12:51 +0300

    added back button overide method

--- app/src/main/java/com/john/jabavu/MainActivity.java
+++ app/src/main/java/com/john/jabavu/MainActivity.java
@@ -39,6 +39,12 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onBackPressed() {
+
+        return;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
